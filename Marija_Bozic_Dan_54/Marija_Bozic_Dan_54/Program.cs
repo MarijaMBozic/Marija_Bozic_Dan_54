e995cf95041295a30c93eafcfb61487dc21d7e87
MarijaMBozic <60043521+MarijaMBozic@users.noreply.github.com>
2020-08-19 11:56:05 +0200

    Initial commit

---
+++ Marija_Bozic_Dan_54/Marija_Bozic_Dan_54/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marija_Bozic_Dan_54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
